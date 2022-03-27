205c69ffb9f00c8803c7b552c1384d77e52e0427
yikwing <hubeieasy@gmail.com>
2022-03-27 10:56:54 +0800

    feat: 动态权限切换到Result API

--- app/src/main/java/com/yikwing/ykquickdev/permission/InvisibleFragment.kt
+++ app/src/main/java/com/yikwing/ykquickdev/permission/InvisibleFragment.kt
@@ -1,6 +1,8 @@
 package com.yikwing.ykquickdev.permission
 
 import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 
 
@@ -14,24 +16,41 @@ class InvisibleFragment : Fragment() {
 
     fun requestNow(cb: PermissionCallback, vararg permissions: String) {
         callback = cb
-        requestPermissions(permissions, PREMISSIONCODE)
+
+        requestPermissionLaunch.launch(permissions)
+//        requestPermissions(permissions, PREMISSIONCODE)
     }
 
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (requestCode == PREMISSIONCODE) {
-            val deniedList = mutableListOf<String>()
-            for ((index, result) in grantResults.withIndex()) {
-                if (result != PackageManager.PERMISSION_GRANTED) {
-                    deniedList.add(permissions[index])
-                }
-            }
-            val allGranted = deniedList.isEmpty()
-            callback?.let {
-                it(allGranted, deniedList)
+//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+//        if (requestCode == PREMISSIONCODE) {
+//            val deniedList = mutableListOf<String>()
+//            for ((index, result) in grantResults.withIndex()) {
+//                if (result != PackageManager.PERMISSION_GRANTED) {
+//                    deniedList.add(permissions[index])
+//                }
+//            }
+//            val allGranted = deniedList.isEmpty()
+//            callback?.let {
+//                it(allGranted, deniedList)
+//            }
+//        }
+//
+//    }
+
+    private val requestPermissionLaunch = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { grantResults ->
+        val deniedList = mutableListOf<String>()
+
+        for ((permission, isGRANTED) in grantResults) {
+            if (!isGRANTED) {
+                deniedList.add(permission)
             }
         }
 
+        val allGranted = deniedList.isEmpty()
+        callback?.let {
+            it(allGranted, deniedList)
+        }
     }
 
 }
