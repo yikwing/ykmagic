35947f3d1061c6da379d72200c12686b395309a9
yikwing <hubeieasy@gmail.com>
2022-03-28 14:23:15 +0800

    feat: 替换activity为fragment

--- app/src/main/java/com/yikwing/ykquickdev/MainActivity.kt
+++ app/src/main/java/com/yikwing/ykquickdev/MainActivity.kt
@@ -1,116 +1,17 @@
 package com.yikwing.ykquickdev
 
-import android.Manifest
-import android.graphics.Color
-import android.util.Log
-import android.widget.Toast
-import androidx.activity.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import com.yikwing.ykextension.backGroundRadiusColor
-import com.yikwing.ykextension.dp
-import com.yikwing.ykquickdev.databinding.ActivityMainBinding
-import com.yikwing.ykquickdev.db.UserDatabase
-import com.yk.ykpermission.PermissionX
-import com.yk.ykconfig.YkQuickManager
-import com.yk.yknetwork.ApiException
-import com.yk.yknetwork.collectState
-import com.yk.ykproxy.BaseActivity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
-
-
-    private val viewModel by viewModels<MyViewModel>()
-
-    private val userDao by lazy {
-        UserDatabase.getInstance(YkQuickManager.getApplication()).getUserDao()
-    }
-
-    private val chapterDao by lazy {
-        UserDatabase.getInstance(YkQuickManager.getApplication()).getChapterDao()
-    }
-
-
-    override fun initView() {
-        super.initView()
-
-
-        viewModel.initData()
-
-
-//        viewModel.headers.observeState(this) {
-//
-//            onLoading = {
-//                Log.d("headers", "加载中")
-//            }
-//
-//            onSuccess = { data: Headers ->
-//                binding.tvTitle.text = data.userAgent + "\n" + data.traceId
-//            }
-//
-//            onError = { e ->
-//                Log.e("headers", e.message ?: "Not Error")
-//            }
-//
-//        }
-
-
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.headers.collectState {
-                    onLoading = {
-                        Log.d("headers", "加载中")
-                    }
-
-                    onSuccess = { data ->
-                        data?.let {
-
-                            lifecycleScope.launch {
-                                withContext(Dispatchers.IO) {
-                                    chapterDao.insertUser(it)
-                                }
-                            }
-
-                        }
-
-                        binding.tvMainTitle.text = data.toString()
-                    }
-
-                    onError = { e ->
-
-                        when (e) {
-                            is ApiException -> Log.e("headers", "${e.code} === ${e.message}")
-                            else -> Log.e("headers", e.message ?: "Not Error")
-                        }
-
-                    }
-                }
-            }
-        }
-
-
-        binding.tvMainTitle.setOnClickListener {
-
-            it.backGroundRadiusColor(Color.parseColor("#0D60B4"), 16f.dp)
-
-            PermissionX.request(
-                this,
-                Manifest.permission.CALL_PHONE,
-                Manifest.permission.CAMERA,
-            ) { allGranted, deniedList ->
-                if (allGranted) {
-                    Toast.makeText(this, "已全部同意", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, "已拒绝 $deniedList", Toast.LENGTH_SHORT).show()
-                }
-            }
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
         }
     }
-
 }
---
+++ app/src/main/java/com/yikwing/ykquickdev/MainFragment.kt
@@ -0,0 +1,80 @@
+package com.yikwing.ykquickdev
+
+import android.Manifest
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.yikwing.ykquickdev.databinding.MainFragmentBinding
+import com.yk.yknetwork.ApiException
+import com.yk.yknetwork.collectState
+import com.yk.ykpermission.PermissionX
+import com.yk.ykproxy.BaseFragment
+import kotlinx.coroutines.launch
+
+class MainFragment : BaseFragment<MainFragmentBinding>(MainFragmentBinding::inflate) {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel by viewModels<MyViewModel>()
+
+    override fun lazyInit() {
+
+        Log.d("=====", "lazyInit")
+
+        viewModel.initData()
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        Log.d("=====", "onActivityCreated")
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.headers.collectState {
+                    onLoading = {
+                        Log.d("headers", "加载中")
+                    }
+
+                    onSuccess = { data ->
+                        binding.message.text = data.toString()
+                    }
+
+                    onError = { e ->
+
+                        when (e) {
+                            is ApiException -> Log.e("headers", "${e.code} === ${e.message}")
+                            else -> Log.e("headers", e.message ?: "Not Error")
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+        binding.message.setOnClickListener {
+
+            PermissionX.request(
+                requireActivity(),
+                Manifest.permission.CALL_PHONE,
+                Manifest.permission.CAMERA,
+            ) { allGranted, deniedList ->
+                if (allGranted) {
+                    Toast.makeText(context, "已全部同意", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context, "已拒绝 $deniedList", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+
+    }
+
+}
