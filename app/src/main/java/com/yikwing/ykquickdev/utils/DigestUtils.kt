cf2915cab9a0bdd7968d1132606e7710917e0f9f
yikwing <hubeieasy@gmail.com>
2022-04-09 20:24:08 +0800

    feat: 添加散列摘要算法

---
+++ app/src/main/java/com/yikwing/ykquickdev/utils/DigestUtils.kt
@@ -0,0 +1,22 @@
+package com.yikwing.ykquickdev.utils
+
+import java.security.MessageDigest
+
+
+object DigestUtils {
+    const val MD5 = "MD5"
+    const val SHA1 = "SHA-1"
+
+    fun crypto(str: String, algorithm: String): String {
+        return crypto(str.toByteArray(), algorithm)
+    }
+
+    fun crypto(buffer: ByteArray, algorithm: String): String {
+        val cryptoResult = MessageDigest.getInstance(algorithm).run {
+            update(buffer)
+            digest()
+        }
+
+        return cryptoResult.joinToString(separator = "") { byte -> "%02x".format(byte) }
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.1.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.41")
 
         // NOTE: Do not place your application dependencies here; they belong
@@ -21,7 +21,7 @@ buildscript {
 }
 
 plugins {
-    id("com.google.devtools.ksp") version "1.6.10-1.0.4" apply false
+    id("com.google.devtools.ksp") version "1.6.20-1.0.4" apply false
 }
 
 apply(from = "ykext.gradle")
