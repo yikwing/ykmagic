8005552988902c9c3aaae7a3edcecfedf4c19c27
yikwing <hubeieasy@gmail.com>
2022-05-23 16:56:06 +0800

    feat: upgrade jdk to 11

--- app/src/main/java/com/yikwing/ykquickdev/MainActivity.kt
+++ app/src/main/java/com/yikwing/ykquickdev/MainActivity.kt
@@ -2,8 +2,19 @@ package com.yikwing.ykquickdev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlin.properties.Delegates
+import kotlin.reflect.KProperty
 
 class MainActivity : AppCompatActivity() {
+
+    private var backPressTime by Delegates.observable(0L) { _: KProperty<*>, oldValue: Long, newValue: Long ->
+        if (newValue - oldValue < 2000) {
+            finish()
+        } else {
+            Toast.makeText(this, "再按一次返回退出", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +24,9 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+    }
+
+    override fun onBackPressed() {
+        backPressTime = System.currentTimeMillis()
     }
 }
