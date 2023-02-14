0a83a9c883f6fc3ab238793810910a18ad364ac4
yikwing <hubeieasy@gmail.com>
2023-02-14 17:47:50 +0800

    feat: add datastore utils

---
+++ module_extension/src/main/java/com/yikwing/ykextension/NotNullSingleVar.kt
@@ -0,0 +1,32 @@
+package com.yikwing.ykextension
+
+import kotlin.properties.Delegates
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+/**
+ *
+ * companion object {
+ *    var instance by Delegates.notNullSingle<MainApplication>()
+ * }
+ *
+ * */
+private class NotNullSingleVar<T> : ReadWriteProperty<Any?, T> {
+
+    private var value: T? = null
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return value
+            ?: throw IllegalStateException("Property ${property.name} should be initialized before get.")
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        this.value = if (this.value == null) {
+            value
+        } else {
+            throw IllegalStateException("Property ${property.name} already initialized")
+        }
+    }
+}
+
+fun <T : Any> Delegates.notNullSingle(): ReadWriteProperty<Any?, T> = NotNullSingleVar()
