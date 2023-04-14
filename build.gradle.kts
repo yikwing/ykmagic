fd7e5d9b0b95bf3478474068c3e4387c29aa8594
yikwing <hubeieasy@gmail.com>
2023-04-14 12:19:53 +0800

    feat: upgrade agp 8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-buildscript {
-
-    repositories {
-        google()
-        mavenCentral()
-        maven {
-            url = uri("https://jitpack.io")
-        }
-    }
-
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
-}
-
 plugins {
+    id("com.android.application") version "8.0.0" apply false
+    id("com.android.library") version "8.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
 }
 
 apply(from = "config.gradle")
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
-}
