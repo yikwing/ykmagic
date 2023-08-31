8aeca47368c460f96bc2d8dd64f6a18f5a1495ab
yikwing <yikwing@yikwings-Laptop.local>
2023-08-31 13:58:37 +0800

    upgrade hilt to ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.ksp) apply false
-
     alias(libs.plugins.hilt) apply false
 }
 
